e0cc630d8c6068b05b3b5bcdcfbbb6af0a6b307b
Solomon Jacobs <solomonjacobs@protonmail.com>
2025-02-08 11:57:11 +0100

    error reporting

--- src/lox/main.py
+++ src/lox/main.py
@@ -19,34 +19,46 @@ def parse_arguments(args: Sequence[str]) -> Args:
     return Args.model_validate(vars(parser.parse_args(args)))  # type: ignore[misc]
 
 
-def run(source: str) -> None:
-    tokens = scan_tokens(source)
-    for token in tokens:
-        print(token)
+class Lox:
+    def __init__(self) -> None:
+        self.had_error = False
 
+    def error(self, line: int, message: str) -> None:
+        self._report(line, "", message)
 
-def run_file(path: Path) -> None:
-    source = path.read_text()
-    run(source)
+    def _report(self, line: int, where: str, message: str) -> None:
+        print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
+        self.had_error = True
 
+    def _run(self, source: str) -> None:
+        tokens = scan_tokens(source)
+        for token in tokens:
+            print(token)
 
-def run_prompt() -> None:
-    while True:
-        try:
-            line = input("> ")
-        except EOFError:
-            print()
-            break
-        run(line)
+    def run_file(self, path: Path) -> None:
+        source = path.read_text()
+        self._run(source)
+        if self.had_error:
+            sys.exit(65)
+
+    def run_prompt(self) -> None:
+        while True:
+            try:
+                line = input("> ")
+            except EOFError:
+                print()
+                break
+            self._run(line)
+            self.had_error = False
 
 
 def main() -> None:
     args = parse_arguments(sys.argv[1:])
     match args.path:
         case None:
-            run_prompt()
+            Lox().run_prompt()
         case path:
-            run_file(path)
+            Lox().run_file(path)
 
 
 if __name__ == "__main__":
